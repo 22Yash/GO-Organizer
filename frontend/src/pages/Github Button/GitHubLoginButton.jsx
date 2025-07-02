0450d47a9788262bb74fb5729abe4e5de58376cd
22Yash <yashadoke@gmail.com>
2025-07-02 16:02:18 +0530

    deployment

--- frontend/src/pages/Github Button/GitHubLoginButton.jsx
+++ frontend/src/pages/Github Button/GitHubLoginButton.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const GitHubLoginButton = ({ text = "Continue with GitHub", disabled = false }) => {
   const handleLogin = () => {
     if (!disabled) {
-      window.location.href = `${import.meta.env.VITE_API_URL}/api/auth/github`; // your OAuth route
+      window.location.href = "https://go-organizer.onrender.com/api/auth/github"; // your OAuth route
     }
   };
 
