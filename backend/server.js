e4c0e3b202cc118e3947c349880a02f1dd3885da
22Yash <yashadoke@gmail.com>
2025-07-02 13:57:34 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,4 +33,6 @@ app.use("/api/file", fileRoutes);
 
 app.use('/api/reports', reportRoutes);
 
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
